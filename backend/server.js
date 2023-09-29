3396d9948cf972cdc6fdab282728ab8d0b722d7a
Kimbali <kimgarcianton@hotmail.com>
2023-09-29 10:08:28 +0200

    update server for deploy to prod

--- backend/server.js
+++ backend/server.js
@@ -39,17 +39,17 @@ app.use('/api/users', usersRoutes);
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
-// if (process.env.NODE_ENV === 'production') {
-//   app.use(express.static(path.join(__dirname, '/front/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-//   app.get('*', (req, res) =>
-//     res.sendFile(path.resolve(__dirname, 'front', 'build', 'index.html'))
-//   );
-// } else {
-app.get('/', (req, res) => {
-  res.send('API is running....');
-});
-// }
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....');
+  });
+}
 
 app.use(notFound);
 app.use(errorHandler);

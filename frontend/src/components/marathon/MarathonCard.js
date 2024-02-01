a5d34f8d9f66fc046b4dff69341925fc5abf4778
Kimbali <kimgarcianton@hotmail.com>
2024-02-01 09:00:08 +0100

    current maraton go to progress

--- frontend/src/components/marathon/MarathonCard.js
+++ frontend/src/components/marathon/MarathonCard.js
@@ -5,13 +5,20 @@ import { formatDateDayMonth } from '../../utils/formatDate';
 import Button from '../button/Button';
 import IMAGE from '../../styles/img/main-img.png';
 import { useTranslation } from 'react-i18next';
+import frontRoutes from '../../config/frontRoutes';
+import { useNavigate } from 'react-router-dom';
 
-function MarathonCard({ handleSelectMarathon, marathon }) {
+function MarathonCard({ handleSelectMarathon, marathon, isCurrent = false }) {
   const { t } = useTranslation();
+  const navigate = useNavigate();
+
+  const goToProgress = () => {
+    navigate(frontRoutes.progress);
+  };
 
   return (
     <div className='marathon-card'>
-      <Text isSubtitle>{marathon.name}</Text>
+      <Text isSubtitle>{marathon?.name}</Text>
 
       <Space extraSmall />
 
@@ -21,15 +28,21 @@ function MarathonCard({ handleSelectMarathon, marathon }) {
       </div>
 
       <div>
-        {formatDateDayMonth(marathon.startDate)} -{' '}
-        {formatDateDayMonth(marathon.endDate)}
+        {formatDateDayMonth(marathon?.startDate)} -{' '}
+        {formatDateDayMonth(marathon?.endDate)}
       </div>
 
       <Space extraSmall />
 
-      <Button small onClick={() => handleSelectMarathon(marathon)} isPrimary>
-        {t('signUp')}
-      </Button>
+      {isCurrent ? (
+        <Button small onClick={goToProgress} isPrimary>
+          {t('goToProgress')}
+        </Button>
+      ) : (
+        <Button small onClick={() => handleSelectMarathon(marathon)} isPrimary>
+          {t('signUp')}
+        </Button>
+      )}
     </div>
   );
 }
--- frontend/src/pages/UserMarathons.js
+++ frontend/src/pages/UserMarathons.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useGetMarathonsQuery } from '../slices/marathonApiSlice';
 import Text from '../components/text/Text';
@@ -11,7 +11,7 @@ import { useTranslation } from 'react-i18next';
 
 function UserMarathons() {
   const { t } = useTranslation();
-  const { setMarathonId } = useMarathon();
+  const { setMarathonId, marathon } = useMarathon();
   const navigate = useNavigate();
 
   const { data: marathonsData } = useGetMarathonsQuery({ isActive: true });
@@ -21,21 +21,43 @@ function UserMarathons() {
     navigate(`${frontRoutes.main}?${MARATHON_ID}=${marathon._id}`);
   };
 
+  console.log(marathon);
+
   return (
     <div>
       <Text isTitle>{t('yourMarathons')}</Text>
 
       <Space medium />
 
+      <Text isSubtitle>{t('yourCurrentMarathon')}</Text>
+
+      <Space medium />
+
+      <div className='user-marathons'>
+        <MarathonCard
+          marathon={marathon}
+          handleSelectMarathon={handleSelectMarathon}
+          isCurrent
+        />
+      </div>
+
+      <Space big />
+
+      <Text isSubtitle>{t('nextMarathons')}</Text>
+
+      <Space medium />
+
       <div className='user-marathons'>
         {marathonsData?.marathons.length > 0 &&
-          marathonsData?.marathons.map((ele, i) => (
-            <MarathonCard
-              marathon={ele}
-              handleSelectMarathon={handleSelectMarathon}
-              key={`marathon-card-${i}`}
-            />
-          ))}
+          marathonsData?.marathons
+            .filter(ele => ele._id !== marathon._id)
+            .map((ele, i) => (
+              <MarathonCard
+                marathon={ele}
+                handleSelectMarathon={handleSelectMarathon}
+                key={`marathon-card-${i}`}
+              />
+            ))}
       </div>
     </div>
   );

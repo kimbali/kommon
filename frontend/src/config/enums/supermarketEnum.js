2d5beb29c18cfbad25379390014a262de58e3310
Kimbali <kimgarcianton@hotmail.com>
2024-02-03 19:10:26 +0100

    update supermarkets section

--- frontend/src/config/enums/supermarketEnum.js
+++ frontend/src/config/enums/supermarketEnum.js
@@ -2,7 +2,6 @@ import { translate } from '../../traducciones/i18n';
 
 const supermarketEnum = [
   { label: translate('fruit'), value: 'FRUIT' },
-  { label: translate('vegetables'), value: 'VEGETABLES' },
   { label: translate('meat'), value: 'MEAT' },
   { label: translate('fish'), value: 'FISH' },
   { label: translate('dairyEgss'), value: 'DAIRY_EGGS' },
@@ -10,6 +9,7 @@ const supermarketEnum = [
   { label: translate('breakfastSweets'), value: 'BREAKFAST_SWEETS' },
   { label: translate('bakery'), value: 'BAKERY' },
   { label: translate('frozen'), value: 'FROZEN' },
+  { label: translate('fridge'), value: 'FRIDGE' },
   { label: translate('species'), value: 'SPECIES' },
 ];
 

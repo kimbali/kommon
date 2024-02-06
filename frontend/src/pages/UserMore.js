11ed16674cb3b8bbc7d6da0d3c5b5937de7a64ec
Kimbali <kimgarcianton@hotmail.com>
2024-02-06 12:44:24 +0100

    config meditations

--- frontend/src/pages/UserMore.js
+++ frontend/src/pages/UserMore.js
@@ -4,20 +4,25 @@ import { faCalculator, faSpa } from '@fortawesome/free-solid-svg-icons';
 import frontRoutes from '../config/frontRoutes';
 import { useTranslation } from 'react-i18next';
 import Text from '../components/text/Text';
+import { useGetConfigsQuery } from '../slices/configApiSlice';
 
 function UserMore() {
   const { t } = useTranslation();
+
+  const { data: configData } = useGetConfigsQuery({});
 
   return (
     <div>
       <Text isTitle>{t('services')}</Text>
       <nav>
         <ul className='more-links'>
-          <NavLink
-            icon={faSpa}
-            label={t('meditations')}
-            route={frontRoutes.meditations}
-          />
+          {configData && configData[0].activeMeditations && (
+            <NavLink
+              icon={faSpa}
+              label={t('meditations')}
+              route={frontRoutes.meditations}
+            />
+          )}
 
           <NavLink
             icon={faCalculator}
